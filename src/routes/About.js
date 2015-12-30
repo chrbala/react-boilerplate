54701b4bf214b19de442b5447e598ffabc98ad53
Chris Bala <chrbala@gmail.com>
2015-12-30 13:04:40 -0800

    Delete home and about pages from boilerplate

--- src/routes/About.js
+++
@@ -1,4 +0,0 @@
-const About = () =>
-	<div>About</div>
-
-export default About
--- src/routes/root.js
+++ src/routes/root.js
@@ -1,16 +1,12 @@
 import render from 'packages/render'
 
 import App from './App'
-import Home from './Home'
-import About from './About'
 import NoMatch from 'shared/NoMatch'
 
 const routes = (
 	<Route path="/" component={App}>
-    <IndexRoute component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="*" component={NoMatch}/>
-  </Route>
+		<Route path="*" component={NoMatch}/>
+	</Route>
 )
 
 render(routes)
