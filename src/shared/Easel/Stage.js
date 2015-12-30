4e6914bf6d1eaebbe392167092cf45c1730bdaa0
Chris Bala <chrbala@gmail.com>
2015-12-30 00:16:11 -0800

    Stage cleanup

--- src/shared/Easel/Stage.js
+++ src/shared/Easel/Stage.js
@@ -1,23 +1,12 @@
 var { Graphics } = createjs
 
 export default class Stage extends Component {
-	constructor() {
-		super()
-		this.defaultProps = {
-			children: []
-		}
-	}
-
-	componentDidMount() {
-		this.stage = new createjs.Stage(this.canvas)
-		this.stage.update()
-	}
-
 	componentDidUpdate() {
 		this.stage.update()
 	}
 
-	handleClick(e) {
+	componentDidMount() {
+		this.stage = new createjs.Stage(this.canvas)
 		this.forceUpdate()
 	}
 
@@ -26,10 +15,7 @@ export default class Stage extends Component {
 		var { stage } = this
 
 		return (
-			<canvas 
-				onClick={::this.handleClick} 
-				ref={canvas => this.canvas = canvas}
-			>
+			<canvas ref={canvas => this.canvas = canvas}>
 				{stage && React.Children.map(children, child =>
 					React.cloneElement(child, { stage })
 				)}
