da3b6c7b7bac9d35bbee1f4fe5fa7896246b69cc
Chris Bala <chrbala@gmail.com>
2015-12-30 23:03:32 -0800

    Improved physics

--- src/routes/App.js
+++ src/routes/App.js
@@ -32,32 +32,54 @@ class App extends GameObject {
 		var { ball } = this
 		var {
 			radius,
-			position: {
-				x,
-				y
-			},
+			position,
 			velocity
 		} = ball
 		var { innerWidth: width, innerHeight: height } = window
-		var [startX, startY] = [x, y]
+		var [x, y] = [position.x, position.y]
+		var [velocityX, velocityY] = [velocity.x, velocity.y]
+
+		var MAX_VELOCITY = isRunning ? 10 : 5
+		var ACCELERATION = isRunning ? 1.4 : .7
+		var DECELERATION = isRunning ? .1 : .5
+
+		var LEFT = -1
+		var RIGHT = 1
+
+		var buttonDirection = right - left
+		var velocityDirection = (velocity.x > 0) - (velocity.x < 0)
 
 		if (y > height + radius * 2)
 			y = -radius * 2
 
-		var MAX = isRunning ? 10 : 5
-		var ACCELERATION = isRunning ? 1.4 : .7
-		var DECELERATION = isRunning ? .1 : .5
+		var atMaxVelocity = Math.abs(velocity.x) >= MAX_VELOCITY
 
-		var atMaxVelocity = right && velocity.x >= MAX
-			|| left && velocity.x <= -MAX
+		// disallow greater velocities than max
+		if (buttonDirection == velocityDirection && atMaxVelocity)
+			velocityX = MAX_VELOCITY * buttonDirection
 
-		if (atMaxVelocity) {}
-		else if (left || right)
-			velocity.addSelf(new Physics.Vector((right - left) * ACCELERATION))
-		else
-			velocity.set(velocity.x * DECELERATION, velocity.y)
+		// accelerate from standstill
+		else if (buttonDirection && !velocityDirection)
+			velocityX = buttonDirection * ACCELERATION
 
-		if (x != startX || y != startY)
+		// decelerate if no button pressed
+		else if (velocityDirection && !buttonDirection)
+			velocityX = velocity.x * DECELERATION
+
+		// accelerate more in current direction
+		else if (buttonDirection == velocityDirection)
+			velocityX += buttonDirection * ACCELERATION 
+
+		// reverse direction
+		else if (buttonDirection && velocityDirection)
+			velocityX += velocity.x * -1
+
+		// update velocity if it has changed
+		if (velocityX != velocity.x || velocityY != velocity.y)
+			velocity.set(velocityX, velocityY)
+
+		// update position if it has changed
+		if (x != position.x || y != position.y)
 			ball.position.set(x, y)
 
 		return (
